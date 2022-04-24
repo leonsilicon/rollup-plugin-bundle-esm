f2987225ca45c4c6700f588a58b866dfb53f71aa
Leon Si <contact@leonzalion.com>
2022-04-23 22:10:10 -0400

    chore: add typings

--- typings/is-file-esm.d.ts
+++ typings/is-file-esm.d.ts
@@ -1,1 +1,27 @@
-declare module 'is-file-esm';
+declare module 'is-file-esm' {
+	type Result = {
+		esm: boolean;
+		type: string;
+		extType: string;
+		path: string;
+		pkgPath: string;
+	};
+
+	declare const isFileEsm: {
+		constants: {
+			ERR_PATH_MUST_BE_STRING: string;
+			ERR_PATH_MUST_BE_ABSOLUTE: string;
+			ERR_PATH_MUST_EXIST: string;
+			ERR_PATH_MUST_HAVE_VALID_EXT: string;
+		};
+
+		(
+			path: string,
+			callback: (error: Error | null, result?: Result) => void
+		): void;
+		(path: string): Promise<Result>;
+		sync(path: string): Result;
+	};
+
+	export = isFileEsm;
+}
